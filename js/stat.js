8266b9d5c1d96b49db12ff7c1baefe748738c148
danilabohanov123 <danilabohanov123@yandex.ru>
2020-09-25 21:52:34 +0300

    Добавление сообщения

--- js/stat.js
+++ js/stat.js
@@ -7,15 +7,27 @@ const CLOUD_HEIGHT = 270;
 const CLOUD_COLOR = `#ffffff`;
 const SHADOW_SHIFT = 10;
 const SHADOW_COLOR = `rgba(0, 0, 0, 0.7)`;
+const MASSAGE_X = 110;
+const MASSAGE_Y = 30;
+const MASSAGE_ROW_GAP = 20;
+const messageRows = [`Ура вы победили!`, `Список результатов:`];
 
-const renderRect = (ctx, x, y, width, height, color = `#000000`) => {
+const renderRect = (ctx, x, y, width, height, color = `#ffffff`) => {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
 
 };
 
-window.renderStatistics = (ctx, names, times) => {
-  renderRect(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_COLOR);
-  renderRect(ctx, CLOUD_X + SHADOW_SHIFT, CLOUD_Y + SHADOW_SHIFT, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR);
+const printText = (ctx, str, x, y, color = `#000000`, font = `16px PT Mono`) => {
+  ctx.font = font;
+  ctx.fillStyle = color;
+  ctx.fillText(str, x, y);
+};
 
+window.renderStatistics = (ctx, names, times) => {
+  renderRect(ctx, CLOUD_X + SHADOW_SHIFT, CLOUD_Y + SHADOW_SHIFT, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR);
+  renderRect(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_COLOR);
+  for (let i = 0; i < messageRows.length; i++) {
+    printText(ctx, messageRows[i], MASSAGE_X, MASSAGE_Y + i * MASSAGE_ROW_GAP);
+  }
 };
