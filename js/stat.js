c08a328340440eb8f2ad707ded8812f01c46bf32
danilabohanov123 <danilabohanov123@yandex.ru>
2020-09-25 15:22:51 +0300

    Создание файла

---
+++ js/stat.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const renderRect = (x, y, width, height, color = "#000000") => {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+
+};
+
+window.renderStatistics = (ctx, names, times) => {
+    
+};
