8694df66dbf5625dbbc19ebadf81de33f47990ef
danilabohanov123 <danilabohanov123@yandex.ru>
2020-09-25 16:46:50 +0300

    Создание облака и тени

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,20 @@
-"use strict"
+"use strict";
 
-const renderRect = (x, y, width, height, color = "#000000") => {
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_COLOR = `#ffffff`;
+const SHADOW_SHIFT = 10;
+const SHADOW_COLOR = `rgba(0, 0, 0, 0.7)`;
+
+const renderRect = (ctx, x, y, width, height, color = `#000000`) => {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
 
 };
 
 window.renderStatistics = (ctx, names, times) => {
-    
+  renderRect(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_COLOR);
+  renderRect(ctx, CLOUD_X + SHADOW_SHIFT, CLOUD_Y + SHADOW_SHIFT, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR);
 };
