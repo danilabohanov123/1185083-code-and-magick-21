0da4a07cdc19fc0dedb4ff407bfdea97d756baff
danilabohanov123 <danilabohanov123@yandex.ru>
2020-09-26 19:38:26 +0300

    Промежуточные изменения

--- js/stat.js
+++ js/stat.js
@@ -11,6 +11,8 @@ const MASSAGE_X = 110;
 const MASSAGE_Y = 30;
 const MASSAGE_ROW_GAP = 20;
 const messageRows = [`Ура вы победили!`, `Список результатов:`];
+const COLUMN_WIDTH = 40;
+const MAX_HEIGHT = 150;
 
 const renderRect = (ctx, x, y, width, height, color = `#ffffff`) => {
   ctx.fillStyle = color;
@@ -24,10 +26,19 @@ const printText = (ctx, str, x, y, color = `#000000`, font = `16px PT Mono`) => 
   ctx.fillText(str, x, y);
 };
 
+const renderPlayerResult = (ctx, x, y, score, player) => {
+  ctx.fillText(player);
+
+};
+
 window.renderStatistics = (ctx, names, times) => {
   renderRect(ctx, CLOUD_X + SHADOW_SHIFT, CLOUD_Y + SHADOW_SHIFT, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR);
   renderRect(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_COLOR);
   for (let i = 0; i < messageRows.length; i++) {
     printText(ctx, messageRows[i], MASSAGE_X, MASSAGE_Y + i * MASSAGE_ROW_GAP);
+  }
+  const maxTime = times.max();
+  for (let i = 0; i < names.length; i++) {
+    renderPlayerResult(ctx,  )
   }
 };
