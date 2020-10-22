32023491d1c8c520319bccb39a62a1b4f6dac47f
danilabohanov123 <danilabohanov123@yandex.ru>
2020-10-22 18:43:16 +0300

    Испрваление ошибок линтера

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,4 @@
-`use strict`;
+"use strict";
 
 const N = 4;
 
@@ -11,40 +11,41 @@ const setup = document.querySelector(`.setup`);
 setup.classList.remove(`hidden`);
 
 const getRandInt = (minNumber, maxNuber) => {
-    return Math.floor(minNumber +  (maxNuber - minNumber) * Math.random());
+  return Math.floor(minNumber + (maxNuber - minNumber) * Math.random());
 };
 
 const getObject = () => {
-    const hero = {};
-    hero.name = firstNames[getRandInt(0, firstNames.length - 1)] + ` ` + secondNames[getRandInt(0, secondNames.length - 1)];
-    hero.coatColor = coatColors[getRandInt(0, coatColors.length - 1)];
-    hero.eyesColoe = eyesColors[getRandInt(0, eyesColors.length - 1)];
-    return hero;
+  const hero = {};
+  hero.name = firstNames[getRandInt(0, firstNames.length - 1)] + ` ` + secondNames[getRandInt(0, secondNames.length - 1)];
+  hero.coatColor = coatColors[getRandInt(0, coatColors.length - 1)];
+  hero.eyesColoe = eyesColors[getRandInt(0, eyesColors.length - 1)];
+  return hero;
 };
 
 const getArrayObjects = () => {
-    const heroes = [];
-    for (let i = 0; i < N; i++) {
-        const hero = getObject();
-        heroes.push(hero);
-    }
-    return heroes;
+  const heroes = [];
+  for (let i = 0; i < N; i++) {
+    const hero = getObject();
+    heroes.push(hero);
+  }
+  return heroes;
 };
 
 const createDomObject = () => {
-    const fragment = document.createDocumentFragment();
-    const heroes = getArrayObjects();
-    const heroTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
-    for (hero of heroes) {
-        const element = heroTemplate.cloneNode(true);
-        element.children[1].textContent = hero.name;
-        element.children[0].children[0].children[0].children[0].style.fill = hero.coatColor;
-        element.children[0].children[0].children[0].children[2].style.fill = hero.eyesColor;
-        fragment.appendChild(element);
-    }
-    const setupSimilarList = document.querySelector(`.setup-similar-list`);
-    setupSimilarList.appendChild(fragment);
-}
+  const fragment = document.createDocumentFragment();
+  const heroes = getArrayObjects();
+  const heroTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
+  for (let i = 0; i < N; i++) {
+    const hero = heroes[i];
+    const element = heroTemplate.cloneNode(true);
+    element.children[1].textContent = hero.name;
+    element.children[0].children[0].children[0].children[0].style.fill = hero.coatColor;
+    element.children[0].children[0].children[0].children[2].style.fill = hero.eyesColor;
+    fragment.appendChild(element);
+  }
+  const setupSimilarList = document.querySelector(`.setup-similar-list`);
+  setupSimilarList.appendChild(fragment);
+};
 
 createDomObject();
 const setupSimilar = document.querySelector(`.setup-similar`);
